ae04a9f9a2265bbdf658b8297622427d3581572e
GolamMuid <golammuid@gmail.com>
2023-01-18 22:13:24 +0600

    added features

--- src/App.js
+++ src/App.js
@@ -6,69 +6,87 @@ import ModeContext from "./contexts/ModeContext";
 import router from "./routes/Routes";
 
 function App() {
-	const { darkMode } = useContext(ModeContext);
+  const { darkMode } = useContext(ModeContext);
 
-	const themeLight = createTheme({
-		palette: {
-			mode: "light",
-			primary: {
-				main: "#9D82BC",
-				// light: "rgb(24, 33, 64)",
-				// dark: "rgb(24, 33, 64)",
-			},
-			secondary: {
-				main: "#ff4e26",
-			},
-			info: {
-				main: "#F8F6FA",
-			},
-			text: {
-				main: "rgb(72 , 72 , 72)",
-			},
-		},
-		typography: {
-			fontFamily:
-				"Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
-			button: {
-				fontWeight: "600",
-			},
-		},
-	});
+  const themeLight = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#9D82BC",
+        // light: "rgb(24, 33, 64)",
+        // dark: "rgb(24, 33, 64)",
+      },
+      secondary: {
+        main: "#ff4e26",
+      },
+      info: {
+        main: "#F8F6FA",
+      },
+      text: {
+        main: "rgb(72 , 72 , 72)",
+      },
+    },
+    typography: {
+      fontFamily:
+        "Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
+      button: {
+        fontWeight: "600",
+      },
+      h1: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+      h2: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+      h3: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+      h4: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+      h5: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+      h6: {
+        fontFamily: "Josefin Sans,sans-serif",
+      },
+    },
+  });
 
-	const themeDark = createTheme({
-		palette: {
-			mode: "dark",
-			primary: {
-				main: "#9D82BC",
-				// light: "rgb(24, 33, 64)",
-				// dark: "rgb(24, 33, 64)",
-			},
-			secondary: {
-				main: "#ff4e26",
-			},
-			info: {
-				main: "#F8F6FA",
-			},
-			background: {
-				default: "#22253C",
-				paper: "#1d1d25",
-			},
-		},
-		typography: {
-			fontFamily:
-				"Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
-			button: {
-				fontWeight: "600",
-			},
-		},
-	});
+  const themeDark = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#9D82BC",
+        // light: "rgb(24, 33, 64)",
+        // dark: "rgb(24, 33, 64)",
+      },
+      secondary: {
+        main: "#ff4e26",
+      },
+      info: {
+        main: "#9D82BC",
+      },
+      background: {
+        default: "#22253C",
+        paper: "#1d1d25",
+      },
+    },
+    typography: {
+      fontFamily:
+        "Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
+      button: {
+        fontWeight: "600",
+      },
+    },
+  });
 
-	return (
-		<ThemeProvider theme={darkMode ? themeDark : themeLight}>
-			<CssBaseline />
-			<RouterProvider router={router} />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : themeLight}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -1,28 +1,96 @@
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useContext } from "react";
+import ModeContext from "../../contexts/ModeContext";
 import Container from "../../layouts/container/Container";
 
 function Features() {
-	return (
-		<Box
-			sx={{
-				backgroundColor: "info.main",
-				// minHeight: "200px",
-				borderRadius: "30%",
-				padding: "60px 0",
-			}}
-		>
-			<Container>
-				<Box display="flex" alignItems="center" justifyContent="space-between">
-					<Paper sx={{ minHeight: "200px" }}> 40 </Paper>
-					<Paper sx={{ minHeight: "200px" }}> 40 </Paper>
-					<Paper sx={{ minHeight: "200px" }}> 40 </Paper>
-					<Paper sx={{ minHeight: "200px" }}> 40 </Paper>
-				</Box>
-			</Container>
-		</Box>
-	);
+  const { darkMode } = useContext(ModeContext);
+  console.log(darkMode);
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "info.main",
+        // minHeight: "200px",
+        borderRadius: "30%",
+        padding: "60px 0",
+      }}
+    >
+      <Container>
+        <Box
+          display="grid"
+          gridTemplateColumns={{
+            xs: "1fr",
+            md: "1fr 1fr",
+            lg: "1fr 1fr 1fr 1fr",
+          }}
+          gap="20px"
+        >
+          <Paper>
+            <Box padding="20px 10px">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/trainer.png`}
+                alt="Expert Trainers"
+                style={{ height: "90px" }}
+              />
+              <Typography variant="h5" color="text.secondary" padding="10px 0">
+                Expert Trainers
+              </Typography>
+              <Typography variant="p" color="text.primary" padding="10px 0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper>
+            <Box padding="20px 10px">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/certificate.png`}
+                alt="Expert Trainers"
+                style={{ height: "90px" }}
+              />
+              <Typography variant="h5" color="text.secondary" padding="10px 0">
+                Certification
+              </Typography>
+              <Typography variant="p" color="text.primary" padding="10px 0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper>
+            <Box padding="20px 10px">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/scholarship.png`}
+                alt="Expert Trainers"
+                style={{ height: "90px" }}
+              />
+              <Typography variant="h5" color="text.secondary" padding="10px 0">
+                Scholarship
+              </Typography>
+              <Typography variant="p" color="text.primary" padding="10px 0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper>
+            <Box padding="20px 10px">
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/job.png`}
+                alt="Expert Trainers"
+                style={{ height: "90px" }}
+              />
+              <Typography variant="h5" color="text.secondary" padding="10px 0">
+                Job Assistance
+              </Typography>
+              <Typography variant="p" color="text.primary" padding="10px 0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Typography>
+            </Box>
+          </Paper>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
 export default Features;
