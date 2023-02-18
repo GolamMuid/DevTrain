51eb2813bf3ac8fbc6712d9df51e0b856271a8c4
GolamMuid <golammuid@gmail.com>
2023-02-18 21:40:44 +0600

    added bootcamp api

--- src/App.js
+++ src/App.js
@@ -1,153 +1,26 @@
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import ModeContext from "./contexts/ModeContext";
 import router from "./routes/Routes";
+import themeDark from "./themes/themeDark";
+import themeLight from "./themes/themeLight";
 
 function App() {
-	const { darkMode } = useContext(ModeContext);
+  const { darkMode } = useContext(ModeContext);
 
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	const { palette } = createTheme();
-
-	const themeLight = createTheme({
-		palette: {
-			mode: "light",
-			primary: {
-				main: "#ff4e26",
-				// light: "rgb(24, 33, 64)",
-				// dark: "rgb(24, 33, 64)",
-			},
-			secondary: {
-				main: "#9D82BC",
-			},
-			info: {
-				main: "#ffebe7",
-				// main: "rgba(255,78,38, 0.3)",
-			},
-			warning: {
-				main: "#f7bb25",
-			},
-			text: {
-				main: "rgb(72 , 72 , 72)",
-			},
-			infoBlue: palette.augmentColor({
-				color: {
-					main: "#0288d1",
-				},
-			}),
-			navBg: palette.augmentColor({
-				color: {
-					main: "rgba(255, 0, 0, 0.0)",
-					scrolled: "rgba(255, 255, 255, .85)",
-				},
-			}),
-		},
-		typography: {
-			fontFamily:
-				"Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
-			button: {
-				fontWeight: "600",
-			},
-			h1: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h2: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h3: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h4: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h5: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h6: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-		},
-		shape: {
-			borderRadius: 10,
-		},
-	});
-
-	const themeDark = createTheme({
-		palette: {
-			mode: "dark",
-			primary: {
-				main: "#ff4e26",
-				// light: "rgb(24, 33, 64)",
-				// dark: "rgb(24, 33, 64)",
-			},
-			secondary: {
-				main: "#9D82BC",
-			},
-			info: {
-				// main: "#9D82BC",
-				main: "#ff4e26",
-			},
-			warning: {
-				main: "#f7bb25",
-			},
-			background: {
-				default: "#0E1320",
-				paper: "#1D2532",
-			},
-			infoBlue: palette.augmentColor({
-				color: {
-					main: "#0288d1",
-				},
-			}),
-			navBg: palette.augmentColor({
-				color: {
-					main: "rgba(255, 0, 0, 0.0)",
-					scrolled: "rgba(	14,	19,	32, .85)",
-				},
-			}),
-		},
-		typography: {
-			fontFamily:
-				"Inter, Sofia Sans Semi Condensed, Josefin Sans, Roboto, sans-serif",
-			button: {
-				fontWeight: "600",
-			},
-			h1: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h2: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h3: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h4: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h5: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-			h6: {
-				fontFamily: "Josefin Sans,sans-serif",
-			},
-		},
-		shape: {
-			borderRadius: 10,
-		},
-	});
-
-	return (
-		<ThemeProvider theme={darkMode ? themeDark : themeLight}>
-			<CssBaseline />
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-			</QueryClientProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : themeLight}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Features from "../../components/home/Features";
+import Features from "../../components/home/features/Features";
 import Bootcamps from "../../components/home/bootcamps/Bootcamps";
-import Hero from "../../components/home/Hero";
+import Hero from "../../components/home/hero/Hero";
 import Reviews from "../../components/home/reviews/Reviews";
 
 function Homepage() {
